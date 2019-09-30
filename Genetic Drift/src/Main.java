7b3079f0074117125efe2f74f3c5ab4eb36629e2
SebastianPilz <SebastianPilz@10.10.221.16>
2019-09-30 16:10:09 +0200

    hallo

---
+++ Genetic Drift/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+	public static void main(String[] args) {	
+		
+		genetic d = new genetic("8 0 3 1 6 5 -2 4 7");
+
+	}
+
+}
---
+++ Genetic Drift/src/genetic.java
@@ -0,0 +1,18 @@
+
+public class genetic {
+	String[] s;
+
+	public genetic(String string) {
+		
+		s = string.split(" ");
+		
+	}
+	
+	public String[] drift() {
+		
+		
+		return s;
+	}
+	
+	
+}
